d598c129e321a8157612aec87fd42435981c3138
shaggyone <vzagorski@inbox.ru>
2011-06-06 23:52:07 +0800

    Some libfile fixes

--- lib/ems_russia.rb
+++ lib/ems_russia.rb
@@ -1,1 +1,8 @@
-$:.unshift File.expand_path('../lib', __FILE__)
+module EmsRussia
+end
+
+require File.expand_path('../ems_russia/cacher', __FILE__)
+require File.expand_path('../ems_protocol', __FILE__)
+require File.expand_path('../ems_protocol/location', __FILE__)
+require File.expand_path('../ems', __FILE__)
+require File.expand_path('../ems_offline', __FILE__)
